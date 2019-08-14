b9f287de4a4bc15ab58da954a8b39cbffe24085b
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-14 14:25:37 -0300

    Inserindo e alterando Conta utilizando os estados Transient e Managed

--- src/teste/TesteConta.java
+++ src/teste/TesteConta.java
@@ -9,15 +9,15 @@ public class TesteConta {
 
 	public static void main(String[] args) {
 		Conta conta = new Conta();
-		conta.setTitular("Luis");
+		conta.setTitular("Otavio");
 		conta.setAgencia("123");
 		conta.setBanco("BB");
 		conta.setNumero("456");
 		
 		EntityManager em = new JPAUtil().getEntityManager();
-		
 		em.getTransaction().begin();
 		em.persist(conta);
+		conta.setTitular("Felipe");
 		em.getTransaction().commit();
 		
 		em.close();

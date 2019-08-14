4d60f7c1446beeae93654c6ff8c2bf63cabefebf
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-14 16:17:21 -0300

    Criando entidades Categoria, Cliente, trabalhando com relacionamentos entre tabelas utilizando @ManyToOne, @OneToMany, @OneToOne e @ManyToMany

--- src/modelo/Movimentacao.java
+++ src/modelo/Movimentacao.java
@@ -2,6 +2,7 @@ package modelo;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,6 +10,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -31,6 +33,18 @@ public class Movimentacao {
 
 	@ManyToOne
 	private Conta conta;
+	
+
+	@ManyToMany
+	private List<Categoria> categorias;
+	
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categoria) {
+		this.categorias = categoria;
+	}
 	
 	public Conta getConta() {
 		return conta;
---
+++ src/teste/TesteMovimentacoesComCategoria.java
@@ -0,0 +1,56 @@
+package teste;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+
+import modelo.Categoria;
+import modelo.Conta;
+import modelo.Movimentacao;
+import modelo.TipoMovimentacao;
+import util.JPAUtil;
+
+public class TesteMovimentacoesComCategoria {
+
+	public static void main(String[] args) {
+		
+		EntityManager manager = new JPAUtil().getEntityManager();
+		manager.getTransaction().begin();
+		
+		Categoria categoria = new Categoria();
+		categoria.setNome("Viagem");
+		
+		Categoria categoria2 = new Categoria();
+		categoria2.setNome("Negocios");
+				
+		Conta conta = manager.find(Conta.class, 2);
+
+		Movimentacao movimentacao = new Movimentacao();
+		movimentacao.setData(Calendar.getInstance());
+		movimentacao.setDescricao("Viagem a SP");
+		movimentacao.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao.setValor(new BigDecimal("1000.0"));
+		movimentacao.setCategorias(Arrays.asList(categoria, categoria2));
+		
+		Movimentacao movimentacao2 = new Movimentacao();
+		movimentacao2.setData(Calendar.getInstance());
+		movimentacao2.setDescricao("Viagem ao RJ");
+		movimentacao2.setTipo(TipoMovimentacao.SAIDA);
+		movimentacao2.setValor(new BigDecimal("1300.0"));
+		movimentacao2.setCategorias(Arrays.asList(categoria, categoria2));
+		
+		movimentacao.setConta(conta);
+		movimentacao2.setConta(conta);
+		
+		manager.persist(categoria);
+		manager.persist(categoria2);
+		manager.persist(movimentacao);
+		manager.persist(movimentacao2);
+		
+		manager.getTransaction().commit();
+		manager.close();
+	}
+	
+}

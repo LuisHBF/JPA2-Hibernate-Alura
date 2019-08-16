5ddc30a7ac874548375026a8e3034b6fea046da5
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-16 15:48:07 -0300

    Utilizando TypedQuery para garantir a tipagem do retorno do método getResultList

--- src/teste/TesteFuncoesJPQL.java
+++ src/teste/TesteFuncoesJPQL.java
@@ -1,9 +1,9 @@
 package teste;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import modelo.Categoria;
 import modelo.TipoMovimentacao;
@@ -18,13 +18,17 @@ public class TesteFuncoesJPQL {
 		
 		Categoria categoria = manager.find(Categoria.class, 1);
 		
-		String jpql = "SELECT sum(m.valor) from Movimentacao m join m.categorias c where c = :pCategoria AND m.tipo = :pTipo";
-		Query query = manager.createQuery(jpql);
+		String jpql = "SELECT avg(m.valor) from Movimentacao m join m.categorias c where c = :pCategoria AND m.tipo = :pTipo"
+				+ " group by day(m.data), month(m.data), year(m.data)";
+		TypedQuery<Double> query = manager.createQuery(jpql,Double.class);
 		query.setParameter("pCategoria", categoria);
 		query.setParameter("pTipo", TipoMovimentacao.ENTRADA);
 		
-		BigDecimal soma = (BigDecimal) query.getSingleResult();
-		System.out.println("A soma é: " + soma.intValue());
+		List<Double> medias = (List<Double>) query.getResultList();
+			
+		medias.forEach(media -> {
+			System.out.println("media: " + media);
+		});
 		
 		manager.getTransaction().commit();
 		manager.close();

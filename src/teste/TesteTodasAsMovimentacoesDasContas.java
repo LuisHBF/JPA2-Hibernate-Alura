96c663de19a43cb11e73a14f96bed56c81ef41dc
Luis Henrique Buzzo Franco <luishbfranco@hotmail.com>
2019-08-16 15:09:23 -0300

    Fazendo select de todas as Movimentacoes de uma Conta sem Lazy Load

---
+++ src/teste/TesteTodasAsMovimentacoesDasContas.java
@@ -0,0 +1,29 @@
+package teste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import modelo.Conta;
+import util.JPAUtil;
+
+public class TesteTodasAsMovimentacoesDasContas {
+
+	public static void main(String[] args) {
+		
+		EntityManager manager = new JPAUtil().getEntityManager();
+		manager.getTransaction().begin();
+		
+		String jpql = "SELECT c from Conta c JOIN fetch c.movimentacoes";
+		Query query = manager.createQuery(jpql);
+		
+		List<Conta> todasAsContas = query.getResultList();
+		
+		todasAsContas.forEach(conta -> {
+			System.out.println("Titular: " + conta.getTitular());
+			System.out.println("Movimentacoes:");
+			System.out.println(conta.getMovimentacoes());
+		});
+	}
+}
